4111a3aeba39452a88d7289e3f0a080f36b8ef9d
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-05-26 22:50:57 +0200

    new solved task added

---
+++ eric_metiz_learning_python/7.1/7.1.py
@@ -0,0 +1,7 @@
+# 24.05.2022
+
+phraze = 'Tell me please, what brand or car do you prefer.'
+phraze += '\nI will try to find this car for you: '
+
+answer = input(phraze)
+print(f'Just the moment. I will try to find {answer} in our garage...')
