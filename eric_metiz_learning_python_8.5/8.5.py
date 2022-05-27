9ddfe51aa1e201f1790bbf074ecb3f804130d939
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-05-27 14:00:49 +0200

    learning_python_eric_metiz_8.5

---
+++ eric_metiz_learning_python_8.5/8.5.py
@@ -0,0 +1,8 @@
+def describe_city(city, country='Ukraine'):
+    print(f'{city.title()} is in {country.title()}.')
+
+
+describe_city('Los Angeles')
+describe_city('Kyiv')
+describe_city('Amsterdam')
+
