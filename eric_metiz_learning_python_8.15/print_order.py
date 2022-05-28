4a9f656c210633285f34fbc9e5b64763e46f874b
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-05-28 10:00:54 +0200

    learning_python_eric_metiz_8.15

---
+++ eric_metiz_learning_python_8.15/print_order.py
@@ -0,0 +1,4 @@
+import printing_functions as pf
+
+components = ['tomato', 'cheese', 'tuna']
+pf.order_maker(*components)
---
+++ eric_metiz_learning_python_8.15/printing_functions.py
@@ -0,0 +1,6 @@
+def order_maker(*args):
+    print('Your sandwich contain:')
+    for i in args:
+        print(f'  -{i}')
+
+    print('It will be ready for you in couple minutes!')
